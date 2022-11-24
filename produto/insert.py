7aa45fb7c3667d96e04e4c2fcf4b57badd15f344
Nicolas Lima <nichollaslimma734@gmail.com>
2022-11-24 12:57:36 -0300

    atualizado insert de produto para salvar o vendedor do produto.

--- produto/insert.py
+++ produto/insert.py
@@ -1,9 +1,22 @@
+from vendedor.select import buscar_vendedores
+
 from datetime import date
 import uuid
 
 def inserir_produto(session):
     dataAtual = date.today()
     execucao = True
+
+    buscar_vendedores(session)
+
+    id_vendedor = input(str("Digite o nome do vendedor que deseja cadastrar um produto: "))
+
+    resultado_busca = session.execute(f"select * from vendedores where id = '{id_vendedor}'")
+
+    for vendedor in resultado_busca:
+        print("vendedor encontrado...")
+        dict = { 'id':vendedor.id, 'nome':vendedor.nome, 'cnpj':vendedor.cnpj, 'email':vendedor.email}
+
     while execucao:
 
         nome = input(str("Digite o nome do produto: "))
@@ -14,11 +27,11 @@ def inserir_produto(session):
 
         session.execute("""
                 insert into produtos
-                    (id, nome, descricao, preco, quantidade, data_postagem )
+                    (id, nome, descricao, preco, quantidade, data_postagem, vendedor )
                 values
-                    (%s,%s,%s,%s,%s,%s)
+                    (%s,%s,%s,%s,%s,%s,%s)
         """,
-        (str(uuid.uuid1()),nome, descricao, preco, quantidade, data_postagem))
+        (str(uuid.uuid1()),nome, descricao, preco, quantidade, data_postagem, str(dict)))
 
         opcao = input(str("Deseja cadastrar outro produto ? [SIM/NAO] "))
 
