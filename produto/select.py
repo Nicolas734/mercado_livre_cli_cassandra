5c9d87dcb1d880a6d5330afd57dd33ea7e0b0890
Nicolas Lima <nichollaslimma734@gmail.com>
2022-11-24 15:38:00 -0300

    atualizado select produtos para mostrar o vendedor do produto.

--- produto/select.py
+++ produto/select.py
@@ -1,8 +1,12 @@
+import json
+
 def buscar_produtos(session):
 
     print("Listagem dos produtos...\n")
 
     for produto in session.execute("select * from produtos"):
+        vendedor = json.loads(produto.vendedor.replace("\'", "\""))
+        print(vendedor['nome'])
         print("|")
         print(f'| id: {produto.id}')
         print(f'| Nome: {produto.nome}')
@@ -10,4 +14,6 @@ def buscar_produtos(session):
         print(f'| Preço: {produto.preco}')
         print(f'| Quantidade de produtos em estoque: {produto.quantidade}')
         print(f'| Data de postagem do produto: {produto.data_postagem}')
-        print("|")
+        print(" | Informações do vendedor: {nome}, email: {email}, cnpj: {cnpj}".format(
+            nome = vendedor['nome'], email = vendedor['email'], cnpj = vendedor['cnpj']
+        ))
